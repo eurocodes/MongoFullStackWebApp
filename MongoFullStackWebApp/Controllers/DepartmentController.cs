44df0dd3283c36e96a093c959872206c78cb607f
Ugo Umeoke <laryhug209@yahoo.com>
2021-08-03 18:15:20 +0100

    Add project files.

---
+++ MongoFullStackWebApp/Controllers/DepartmentController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using MongoFullStackWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoFullStackWebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+            var dbList = dbClient.GetDatabase("testdb").GetCollection<Department>("Department").AsQueryable();
+            return new JsonResult(dbList);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Department dept)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+            int lastdeptId = dbClient.GetDatabase("testdb").GetCollection<Department>("Department").AsQueryable().Count();
+            dept.DepartmentId = lastdeptId + 1;
+
+            dbClient.GetDatabase("testdb").GetCollection<Department>("Department").InsertOne(dept);
+
+            return new JsonResult("Added susseccfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Department dept)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+            var filter = Builders<Department>.Filter.Eq("DepartmentId", dept.DepartmentId);
+            var update = Builders<Department>.Update.Set("DepartmentName", dept.DepartmentName);
+
+            dbClient.GetDatabase("testdb").GetCollection<Department>("Department").UpdateOne(filter, update);
+            return new JsonResult("Updated susseccfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            MongoClient dbClient = new MongoClient(_configuration.GetConnectionString("EmployeeAppCon"));
+            var filter = Builders<Department>.Filter.Eq("DepartmentId", id);
+
+            dbClient.GetDatabase("testdb").GetCollection<Department>("Department").DeleteOne(filter);
+            return new JsonResult("Deleted susseccfully");
+        }
+    }
+}
---
+++ MongoFullStackWebApp/Startup.cs
@@ -0,0 +1,86 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoFullStackWebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Enable CORS
+            services.AddCors(cors =>
+            {
+                cors.AddPolicy("AllowOrigin", option => option.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+            //JSON Serializer
+            services.AddControllersWithViews().AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+                .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+            services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                 options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "App By Ugo",
+                    Version = "v1",
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Enable CORS
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "My App"));
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), "Photos")),
+                RequestPath = "/Photos"
+            });
+        }
+    }
+}
